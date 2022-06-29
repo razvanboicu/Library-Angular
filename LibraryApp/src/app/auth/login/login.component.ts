312c98807251bb36d0ab8006ab35ddb55b806b80
Razvan Boicu <razvanonline@yahoo.com>
2022-06-29 17:58:22 +0300

    login linked to the new API, bug fixed with the responseType: 'text'

--- LibraryApp/src/app/auth/login/login.component.ts
+++ LibraryApp/src/app/auth/login/login.component.ts
@@ -11,12 +11,13 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  
+  rememberMe: boolean = false;
   logInForm!: FormGroup;
   constructor(private router: Router, private _snackBar: MatSnackBar, private http:HttpClient) {}
 
   ngOnInit(): void {
     this.initializeForm();
-
   }
 
   initializeForm() {
@@ -27,22 +28,29 @@ export class LoginComponent implements OnInit {
   }
 
   logIn() {
-    //todo
-    const body={
-      email: this.logInForm.get('email')?.value,
-      password: this.logInForm.get('password')?.value
-      // email:"eve.holt@reqres.in",
-      // password:"cityslicka"
-    }
-    this.http.post("https://reqres.in/api/login", body).subscribe(
-      (res:any)=>{
+        // const body = {
+    //   email: this.logInForm.get('email')?.value,
+    //   password: this.logInForm.get('password')?.value
+    //   // email:"eve.holt@reqres.in",
+    //   // password:"cityslicka"
+    // }
+    
+    let body = new FormData();
+    body.append('Username', this.logInForm.get('email')?.value);
+    body.append('Password', this.logInForm.get('password')?.value);
+
+    this.http.post("https://localhost:7153/users/login", body,{responseType:'text'}).subscribe(
+      (res:any) => {
         console.log(res)
         this.router.navigateByUrl('');
         this._snackBar.open('Log In Successfully!', '', {
         duration: 2000,
-    });
-    window.localStorage.setItem("token", res.token)
-      },(error)=>{
+        
+      });
+      //console.log(res.token);
+      window.localStorage.setItem("token", res.token)
+    
+    },(error)=>{
         console.error(error)
         this._snackBar.open(error.error.error, '',{
           duration:2000,
